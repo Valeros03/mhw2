c5dda06fa15a223e60e614a9d132ce0b0339b6d2
Seby Fichera <120064924+Valeros03@users.noreply.github.com>
2025-04-13 17:29:28 +0200

    Update mhw2.js

--- mhw2.js
+++ mhw2.js
@@ -77,3 +77,64 @@ function showInputContainer(){
 searchButton.addEventListener('click', showInputContainer);
 document.addEventListener('click', hideDropDown);
 menu.addEventListener('click', showDropDown);
+
+
+function createImage(src) {
+    const image = document.createElement('img');
+    image.src = src;
+    return image;
+  }
+  
+function createParagraph(text){
+    const newText = document.createElement('p');
+    newText.textContent = text;
+    newText.classList.add('open-sans');
+    newText.classList.add('white-text');
+    newText.style.margin = 16 + 'px';
+    return newText;
+
+}
+
+function createH2(text){
+
+    const newText = document.createElement('h2');
+    newText.textContent = text;
+    newText.classList.add('open-sans');
+    newText.classList.add('white-text');
+    newText.style.margin = 16 + 'px';
+    return newText;
+
+}
+
+function onThumbnailClick(event) {
+    
+    const imageSrc = event.currentTarget.querySelector('img');
+    const nomeArtist = createH2(event.currentTarget.querySelector('.artist-name').textContent);
+    const description = createParagraph(event.currentTarget.dataset.description);
+
+    const image = createImage(imageSrc.src);
+    document.body.classList.add('no-scroll');
+    modalView.style.top = window.pageYOffset + 'px';
+    artistInfo.appendChild(nomeArtist);
+    artistInfo.appendChild(image);
+    artistInfo.appendChild(description);
+    modalView.classList.remove('hidden');
+
+  }
+  
+  function onModalClick() {
+    document.body.classList.remove('no-scroll');
+    modalView.classList.add('hidden');
+    artistInfo.innerHTML = '';
+  }
+  
+const artistCategory = document.querySelector('#artist-category');
+const artistList = artistCategory.querySelectorAll('div.element-show');
+
+for(let element of artistList){
+    element.addEventListener('click', onThumbnailClick);
+}
+
+const modalView = document.querySelector('#modal-view');
+const artistInfo = document.querySelector('#artist-info');
+modalView.addEventListener('click', onModalClick);
